3537e5dc8b442bba9d0e7762a32a542b4f9e1d32
iuxs <1210938295@qq.com>
2016-09-09 06:13:07 +0000

    iptabels rule creater for SS by chnrouters

--- chnroutes.py
+++ chnroutes.py
@@ -211,6 +211,7 @@ iptables -t nat -A SHADOWSOCKS -d 240.0.0.0/4 -j RETURN
     iptables -t nat -A SHADOWSOCKS -d 210.129.16.166/32 -p tcp -j RETURN
     iptables -t nat -A SHADOWSOCKS -d 124.247.248.0/24 -p tcp -j RETURN
     iptables -t nat -A SHADOWSOCKS -d 117.18.65.0/24 -p tcp -j RETURN
+    iptables -t nat -A SHADOWSOCKS -d 203.178.92.0/24 -p tcp -j RETURN
     """)
     upscript_footer=textwrap.dedent("""\
 
